3b3ad53b175eabe41e243f8b8b1f065089a7f0c4
Adithya G <121248641+adithyag1@users.noreply.github.com>
2026-03-29 18:03:55 +0530

    Clean up Config class by removing unused properties
    Removed unused properties from Config subclasses.

--- configs.py
+++ configs.py
@@ -1,7 +1,6 @@
 import json
 import torch
 
-
 class Config(object):
     def __init__(self, config, file_path="configs.json"):
         with open(file_path) as config_file:
@@ -56,14 +55,6 @@ class Data(Config):
     def model(self):
         return self.get_property('model')
 
-    @property
-    def tokens(self):
-        return self.get_property('tokens')
-
-    @property
-    def w2v(self):
-        return self.get_property('w2v')
-
 
 class Paths(Data):
     def __init__(self):
@@ -78,14 +69,6 @@ class Files(Data):
     def __init__(self):
         super().__init__('files')
 
-    @property
-    def tokens(self):
-        return self.get_property('tokens')
-
-    @property
-    def w2v(self):
-        return self.get_property('w2v')
-
 
 class Embed(Config):
     def __init__(self):
@@ -94,14 +77,6 @@ class Embed(Config):
     @property
     def nodes_dim(self):
         return self.get_property('nodes_dim')
-
-    @property
-    def w2v_args(self):
-        return self.get_property('word2vec_args')
-
-    @property
-    def edge_type(self):
-        return self.get_property('edge_type')
 
 
 class Process(Config):
@@ -119,10 +94,6 @@ class Process(Config):
     @property
     def batch_size(self):
         return self.get_property('batch_size')
-
-    @property
-    def dataset_ratio(self):
-        return self.get_property('dataset_ratio')
 
     @property
     def shuffle(self):
@@ -155,32 +126,3 @@ class Process(Config):
     @property
     def weight_1(self):
         return self.get_property('weight_1')
-
-
-class Devign(Config):
-    def __init__(self):
-        super().__init__('devign')
-
-    @property
-    def learning_rate(self):
-        return self.get_property('learning_rate')
-
-    @property
-    def weight_decay(self):
-        return self.get_property('weight_decay')
-
-    @property
-    def loss_lambda(self):
-        return self.get_property('loss_lambda')
-
-    @property
-    def weight_0(self):
-        return self.get_property('weight_0')
-
-    @property
-    def weight_1(self):
-        return self.get_property('weight_1')
-
-    @property
-    def model(self):
-        return self.get_property('model')
